56b503d78bb0cde159760747e5895c2b5f6cf831
James Johnston <jamesaj2323@gmail.com>
2021-05-26 17:41:59 +1000

    find gateway code

---
+++ packet_gen.py
@@ -0,0 +1,19 @@
+import socket
+import struct
+
+import scapy
+
+# Helper method to obtain IP of our router
+def get_default_gateway_linux():
+    """Read the default gateway directly from /proc."""
+    with open("/proc/net/route") as fh:
+        for line in fh:
+            fields = line.strip().split()
+            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
+                # If not default route or not RTF_GATEWAY, skip it
+                continue
+
+            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
+            
+
+
